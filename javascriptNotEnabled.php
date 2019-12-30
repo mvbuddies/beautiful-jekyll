eafc537348ebd9c8f6c02f399f745f8261212757
Jonas <jonas.klugmann@gmail.com>
2019-12-30 06:33:10 +0100

    Init

---
+++ javascriptNotEnabled.php
@@ -0,0 +1,2 @@
+<h3> You must have JavaScript enabled in order to use the Website. Please
+     enable JavaScript and then <a href="https://mvbuddies.de">reload</a> this page in order to continue. </h3>
---
+++ join.md
@@ -0,0 +1,5 @@
+---
+layout: page
+title: In Arbeit
+subtitle: Bald mit Inhalt
+---
