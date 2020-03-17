db3285adc074c421a0028dd0aa66ec8b3d905892
Jonas <24591995+j0nas500@users.noreply.github.com>
2020-03-17 13:52:53 +0100

    Delete javascriptNotEnabled.php

--- javascriptNotEnabled.php
+++
@@ -1,2 +0,0 @@
-<h3> You must have JavaScript enabled in order to use the Website. Please
-     enable JavaScript and then <a href="https://mvbuddies.de">reload</a> this page in order to continue. </h3>
